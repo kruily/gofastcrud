2d79c2c6a8cfe9d8cf7955a1aef16020cef834d5
惊修 <1693186883@qq.com>
2024-12-10 13:17:05 +0800

    增加redis

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/go-openapi/spec v0.21.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/joho/godotenv v1.5.1
+	github.com/redis/go-redis/v9 v9.7.0
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.9.0
@@ -72,7 +73,9 @@ require (
 require (
 	github.com/bmatcuk/doublestar/v4 v4.6.1 // indirect
 	github.com/casbin/govaluate v1.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/glebarez/go-sqlite v1.20.3 // indirect
 	github.com/glebarez/sqlite v1.7.0 // indirect
---
+++ pkg/cache/redis.go
@@ -0,0 +1,217 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+	"strconv"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+// RedisCache Redis缓存实现
+type RedisCache struct {
+	client *redis.Client
+}
+
+// RedisConfig Redis配置
+type RedisConfig struct {
+	Host     string
+	Port     int
+	Password string
+	DB       int
+	PoolSize int
+}
+
+// NewRedisCache 创建Redis缓存实例
+func NewRedisCache(config RedisConfig) (*RedisCache, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     config.Host + ":" + strconv.Itoa(config.Port),
+		Password: config.Password,
+		DB:       config.DB,
+		PoolSize: config.PoolSize,
+	})
+
+	// 测试连接
+	ctx := context.Background()
+	if err := client.Ping(ctx).Err(); err != nil {
+		return nil, err
+	}
+
+	return &RedisCache{
+		client: client,
+	}, nil
+}
+
+// Set 设置缓存
+func (c *RedisCache) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	bytes, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	return c.client.Set(ctx, key, bytes, expiration).Err()
+}
+
+// Get 获取缓存
+func (c *RedisCache) Get(ctx context.Context, key string, value interface{}) error {
+	bytes, err := c.client.Get(ctx, key).Bytes()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bytes, value)
+}
+
+// Delete 删除缓存
+func (c *RedisCache) Delete(ctx context.Context, key string) error {
+	return c.client.Del(ctx, key).Err()
+}
+
+// Exists 检查key是否存在
+func (c *RedisCache) Exists(ctx context.Context, key string) (bool, error) {
+	n, err := c.client.Exists(ctx, key).Result()
+	return n > 0, err
+}
+
+// MSet 批量设置
+func (c *RedisCache) MSet(ctx context.Context, pairs map[string]interface{}, expiration time.Duration) error {
+	pipe := c.client.Pipeline()
+	for k, v := range pairs {
+		bytes, err := json.Marshal(v)
+		if err != nil {
+			return err
+		}
+		pipe.Set(ctx, k, bytes, expiration)
+	}
+	_, err := pipe.Exec(ctx)
+	return err
+}
+
+// MGet 批量获取
+func (c *RedisCache) MGet(ctx context.Context, keys []string) (map[string]string, error) {
+	values, err := c.client.MGet(ctx, keys...).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[string]string)
+	for i, key := range keys {
+		if values[i] != nil {
+			result[key] = values[i].(string)
+		}
+	}
+	return result, nil
+}
+
+// MDelete 批量删除
+func (c *RedisCache) MDelete(ctx context.Context, keys []string) error {
+	return c.client.Del(ctx, keys...).Err()
+}
+
+// LPush 左推入列表
+func (c *RedisCache) LPush(ctx context.Context, key string, values ...interface{}) error {
+	return c.client.LPush(ctx, key, values...).Err()
+}
+
+// RPush 右推入列表
+func (c *RedisCache) RPush(ctx context.Context, key string, values ...interface{}) error {
+	return c.client.RPush(ctx, key, values...).Err()
+}
+
+// LPop 左弹出列表
+func (c *RedisCache) LPop(ctx context.Context, key string) (string, error) {
+	return c.client.LPop(ctx, key).Result()
+}
+
+// RPop 右弹出列表
+func (c *RedisCache) RPop(ctx context.Context, key string) (string, error) {
+	return c.client.RPop(ctx, key).Result()
+}
+
+// LRange 获取列表范围
+func (c *RedisCache) LRange(ctx context.Context, key string, start, stop int64) ([]string, error) {
+	return c.client.LRange(ctx, key, start, stop).Result()
+}
+
+// SAdd 添加集合成员
+func (c *RedisCache) SAdd(ctx context.Context, key string, members ...interface{}) error {
+	return c.client.SAdd(ctx, key, members...).Err()
+}
+
+// SMembers 获取集合所有成员
+func (c *RedisCache) SMembers(ctx context.Context, key string) ([]string, error) {
+	return c.client.SMembers(ctx, key).Result()
+}
+
+// SRem 删除集合成员
+func (c *RedisCache) SRem(ctx context.Context, key string, members ...interface{}) error {
+	return c.client.SRem(ctx, key, members...).Err()
+}
+
+// SIsMember 判断是否是集合成员
+func (c *RedisCache) SIsMember(ctx context.Context, key string, member interface{}) (bool, error) {
+	return c.client.SIsMember(ctx, key, member).Result()
+}
+
+// ZAdd 添加有序集合成员
+func (c *RedisCache) ZAdd(ctx context.Context, key string, score float64, member interface{}) error {
+	return c.client.ZAdd(ctx, key, redis.Z{Score: score, Member: member}).Err()
+}
+
+// ZRange 获取有序集合范围
+func (c *RedisCache) ZRange(ctx context.Context, key string, start, stop int64) ([]string, error) {
+	return c.client.ZRange(ctx, key, start, stop).Result()
+}
+
+// ZRangeByScore 按分数获取有序集合范围
+func (c *RedisCache) ZRangeByScore(ctx context.Context, key string, min, max float64) ([]string, error) {
+	opt := &redis.ZRangeBy{
+		Min: strconv.FormatFloat(min, 'f', -1, 64),
+		Max: strconv.FormatFloat(max, 'f', -1, 64),
+	}
+	return c.client.ZRangeByScore(ctx, key, opt).Result()
+}
+
+// HSet 设置哈希字段
+func (c *RedisCache) HSet(ctx context.Context, key, field string, value interface{}) error {
+	return c.client.HSet(ctx, key, field, value).Err()
+}
+
+// HGet 获取哈希字段
+func (c *RedisCache) HGet(ctx context.Context, key, field string) (string, error) {
+	return c.client.HGet(ctx, key, field).Result()
+}
+
+// HGetAll 获取所有哈希字段
+func (c *RedisCache) HGetAll(ctx context.Context, key string) (map[string]string, error) {
+	return c.client.HGetAll(ctx, key).Result()
+}
+
+// HDel 删除哈希字段
+func (c *RedisCache) HDel(ctx context.Context, key string, fields ...string) error {
+	return c.client.HDel(ctx, key, fields...).Err()
+}
+
+// Incr 递增
+func (c *RedisCache) Incr(ctx context.Context, key string) (int64, error) {
+	return c.client.Incr(ctx, key).Result()
+}
+
+// Decr 递减
+func (c *RedisCache) Decr(ctx context.Context, key string) (int64, error) {
+	return c.client.Decr(ctx, key).Result()
+}
+
+// Expire 设置过期时间
+func (c *RedisCache) Expire(ctx context.Context, key string, expiration time.Duration) error {
+	return c.client.Expire(ctx, key, expiration).Err()
+}
+
+// TTL 获取剩余时间
+func (c *RedisCache) TTL(ctx context.Context, key string) (time.Duration, error) {
+	return c.client.TTL(ctx, key).Result()
+}
+
+// Close 关闭连接
+func (c *RedisCache) Close() error {
+	return c.client.Close()
+}
